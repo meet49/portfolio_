9060fa174fb9210d7ce4d51124d699a520331577
meet49 <meetsojitra49@gmail.com>
2024-06-30 14:03:51 +0530

    change on the navbar

--- src/app/componets/Navbar.jsx
+++ src/app/componets/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-gray-800 text-white p-4">
+    <nav className="bg-white text-black p-4 xl:px-10">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-xl font-bold">
           Meet Sojitra
